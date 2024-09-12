e95e8d8d9d4f65b9957ff0ef3db39b03a2764f7e
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-09-12 10:46:13 -0500

    The file PokemonListData was created.

---
+++ Pokedex_MVC/Model/PokemonListData.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonListData.swift
+//  Pokedex_MVC
+//
+//  Created by Juan Pablo Lasprilla Correa on 12/09/24.
+//
+
+import Foundation
+
+struct PokemonListData: Decodable {
+    let next: String
+    let previous: String
+    let results: [ResultsPokemonList]
+}
+
+struct ResultsPokemonList: Decodable{
+    let name: String
+    let url: String
+}
