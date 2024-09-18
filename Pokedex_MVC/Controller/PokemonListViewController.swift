5b5d4825a549d8f2f0c860530793a52bbd3d569c
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-09-18 07:20:20 -0500

    The "PokemonImageManager" was created to provide the "Pokemon List ViewController" with the image data of the pokemon through a delegate.

--- Pokedex_MVC/Controller/PokemonListViewController.swift
+++ Pokedex_MVC/Controller/PokemonListViewController.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
-class PokemonListViewController: UIViewController, PokemonListManagerDelegate {
+class PokemonListViewController: UIViewController, PokemonListManagerDelegate, PokemonImageProviderDelegate {
 
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
     
     var pokemonListManager = PokemonListManager()
+    var pokemonImageManager = PokemonImageManager()
+    
+    var PokemonList: [PokemonOnListModel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         pokemonListManager.delegate = self
+        pokemonImageManager.delegate = self
+        
         pokemonListManager.fetchPokemonList(limit: 3, offset: 0)
+        print(PokemonList.count)
     }
     
     func didUpdatePokemonList(_ pokemonListManager: PokemonListManager, pokemonList: [PokemonOnListModel]) {
-        print(pokemonList[0].name)
+        DispatchQueue.main.async {
+
+            self.pokemonName.text = pokemonList[0].name
+            self.pokemonImageManager.fetchImage(from: pokemonList[0].sprite)
+            
+        }
     }
     
     
     func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    func didReceiveImage(_ data: Data) {
+        DispatchQueue.main.async {
+            let image = UIImage(data: data)!
+            self.pokemonImage.image = image
+        }
+    }
+    
+    func didFailToLoadImage(error: Error) {
         print(error)
     }
 
---
+++ Pokedex_MVC/Model/PokemonImageManager.swift
@@ -0,0 +1,56 @@
+//
+//  PokemonImageManager.swift
+//  Pokedex_MVC
+//
+//  Created by Juan Pablo Lasprilla Correa on 18/09/24.
+//
+
+import Foundation
+
+protocol PokemonImageProviderDelegate: AnyObject {
+    // Método que se llama cuando la imagen es descargada exitosamente
+    func didReceiveImage(_ data: Data)
+    
+    // Método que se llama si ocurre un error durante la descarga de la imagen
+    func didFailToLoadImage(error: Error)
+}
+
+struct PokemonImageManager {
+    
+    // Definimos una propiedad weak para evitar ciclos de retención
+    weak var delegate: PokemonImageProviderDelegate?
+
+    // Método para descargar la imagen desde una URL
+    func fetchImage(from urlString: String) {
+        // Verificar si la URL es válida
+        guard let url = URL(string: urlString) else {
+            print("URL inválida")
+            return
+        }
+        
+        // Descargar la imagen de manera asincrónica
+        URLSession.shared.dataTask(with: url) {  (data, response, error) in
+            // Manejar errores de red
+            if let error = error {
+                DispatchQueue.main.async {
+                    delegate?.didFailToLoadImage(error: error)
+                }
+                return
+            }
+            
+            // Verificar si se ha recibido data válida
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    delegate?.didFailToLoadImage(error: NSError(domain: "ImageError", code: -1, userInfo: nil))
+                }
+                return
+            }
+            
+            // Notificar al delegate que la imagen fue recibida exitosamente
+            DispatchQueue.main.async {
+                delegate?.didReceiveImage(data)
+            }
+        }.resume()
+    }
+    
+}
